6b18a2e915cc5bac4cb3c3cedbfb083374aff46c
TitusWongCN <wzhwno1@163.com>
2020-01-12 17:20:11 +0800

    update try_face: face_locations & face_landmarks functions

--- WearChristmasHat/try_face.py
+++ WearChristmasHat/try_face.py
@@ -1,23 +1,30 @@
 # -*- coding=utf-8 -*-
 import face_recognition
 import cv2
+import copy
 
 
-image = face_recognition.load_image_file('face1.jpg')
-face_locations = face_recognition.face_locations(image)
-face_landmarks = face_recognition.face_landmarks(image, face_locations)
+faces = ['face1.png', 'face2.jpg', 'face3.png']
 
-for each in face_landmarks:
-    for i in each.keys():
-        # left_eyebrow[(141, 160), (149, 159), (157, 163), (163, 168), (169, 174)]
-        # right_eyebrow[(186, 182), (197, 184), (206, 186), (213, 189), (218, 195)]
-        # left_eye[(146, 176), (151, 176), (157, 180), (161, 187), (154, 185), (148, 182)]
-        # right_eye[(185, 198), (191, 196), (198, 199), (203, 203), (196, 204), (190, 202)]
-        if 'eye' in i:
-            print(i, each[i])
-            for any in each[i]:
-                image = cv2.circle(image, any, 3, (0,0,255), -1)
+for face in faces:
+    image = face_recognition.load_image_file(face)
+    face_locations = face_recognition.face_locations(image)
 
-# Display the resulting image
-cv2.imshow('image', image[:,:,::-1])
-cv2.waitKey(0)
+    rects_face = copy.deepcopy(image)
+    for top, right, bottom, left in face_locations:
+        cv2.rectangle(rects_face, (left, top), (right, bottom), (0, 0, 255), 2)
+
+    points_face = copy.deepcopy(image)
+    face_landmarks = face_recognition.face_landmarks(image, face_locations)
+    print(face_landmarks)
+    for each in face_landmarks:
+        for i in each.keys():
+            if '' in i:
+                # print(i, each[i])
+                for any in each[i]:
+                    points_face = cv2.circle(points_face, any, 3, (0,0,255), -1)
+
+    # Display the resulting image
+    cv2.imshow('points_face', points_face[:,:,::-1])
+    cv2.imshow('rects_face', rects_face[:,:,::-1])
+    cv2.waitKey(0)
