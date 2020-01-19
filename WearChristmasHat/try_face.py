7510c74678950f738704eb11304ae9c95f3908ce
TitusWongCN <wzhwno1@163.com>
2020-01-19 08:33:19 +0800

    update code history

--- WearChristmasHat/try_face.py
+++ WearChristmasHat/try_face.py
@@ -1,30 +1,36 @@
-# -*- coding=utf-8 -*-
-import face_recognition
-import cv2
-import copy
+# # -*- coding=utf-8 -*-
+# import face_recognition
+# import cv2
+# import copy
+#
+#
+# faces = ['face1.png', 'face2.jpg', 'face3.png']
+#
+# for face in faces:
+#     image = face_recognition.load_image_file(face)
+#     face_locations = face_recognition.face_locations(image)
+#
+#     rects_face = copy.deepcopy(image)
+#     for top, right, bottom, left in face_locations:
+#         cv2.rectangle(rects_face, (left, top), (right, bottom), (0, 0, 255), 2)
+#
+#     points_face = copy.deepcopy(image)
+#     face_landmarks = face_recognition.face_landmarks(image, face_locations)
+#     print(face_landmarks)
+#     for each in face_landmarks:
+#         for i in each.keys():
+#             if '' in i:
+#                 # print(i, each[i])
+#                 for any in each[i]:
+#                     points_face = cv2.circle(points_face, any, 3, (0,0,255), -1)
+#
+#     # Display the resulting image
+#     cv2.imshow('points_face', points_face[:,:,::-1])
+#     cv2.imshow('rects_face', rects_face[:,:,::-1])
+#     cv2.waitKey(0)
 
+from PIL import Image
 
-faces = ['face1.png', 'face2.jpg', 'face3.png']
-
-for face in faces:
-    image = face_recognition.load_image_file(face)
-    face_locations = face_recognition.face_locations(image)
-
-    rects_face = copy.deepcopy(image)
-    for top, right, bottom, left in face_locations:
-        cv2.rectangle(rects_face, (left, top), (right, bottom), (0, 0, 255), 2)
-
-    points_face = copy.deepcopy(image)
-    face_landmarks = face_recognition.face_landmarks(image, face_locations)
-    print(face_landmarks)
-    for each in face_landmarks:
-        for i in each.keys():
-            if '' in i:
-                # print(i, each[i])
-                for any in each[i]:
-                    points_face = cv2.circle(points_face, any, 3, (0,0,255), -1)
-
-    # Display the resulting image
-    cv2.imshow('points_face', points_face[:,:,::-1])
-    cv2.imshow('rects_face', rects_face[:,:,::-1])
-    cv2.waitKey(0)
+image = Image.open('./hat.png')
+image = image.rotate(-15)
+image.show()
--- WearChristmasHat/wear_hat.py
+++ WearChristmasHat/wear_hat.py
@@ -1,23 +1,41 @@
 # -*- coding=utf-8 -*-
 import face_recognition
 from PIL import Image
+import math
 
 
-face_image = face_recognition.load_image_file('face2.jpg')
+def get_distance(point1, point2):
+    return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+
+image_path = './face3.png'
+
+face_image = face_recognition.load_image_file(image_path)
 face_locations = face_recognition.face_locations(face_image)
 face_landmarks = face_recognition.face_landmarks(face_image, face_locations)
 
-human_img = Image.open('./face2.jpg')
+human_img = Image.open(image_path)
 human_img = human_img.convert("RGBA")
+# 圣诞帽相关参数
 hat_img = Image.open("./hat.png")
+hat_brim_ratio = 175.0 / 300.0
 hat_img = hat_img.convert("RGBA")
 
 for face_location, face_landmark in zip(face_locations, face_landmarks):
+    chin = face_landmark['chin']
+    nose_bridge = face_landmark['nose_bridge']
+
+    face_width = get_distance(chin[0], chin[-1])
+    hair_brim = get_distance(nose_bridge[-1], chin[int(len(chin)/2)])
+
+    image_width = int(face_width / hat_brim_ratio)
+
     top, right, bottom, left = face_location
     top -= 10
     head_h = bottom - top  # hight of head
     head_l = right - left  # length of head
     hat_img = hat_img.resize((head_l, head_h))  # convert size of hat
+
+    # hat_img = hat_img.rotate(45)
     hat_region = hat_img
     human_region = (left, top - head_h, right, top)
     human_img.paste(hat_region, human_region, mask=hat_img)
